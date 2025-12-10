9b8340c9c90284f47d8a961fdf9371261360f560
rodrigcasio <rod.casio@outlook.com>
2025-12-10 17:18:36 -0600

    `feat:` Add new cmd placement

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- NEW CUSTOM SETTINGS:
+-- 1. Set command line height to 2 or 3.
+-- Setting it to value > 1 forces Neovim to use the bottom bar
+-- instead of floating window for the cmd line
+vim.opt.cmdheight = 2

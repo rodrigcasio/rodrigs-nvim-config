fc98cfbda965880e73d5af9cef429511810262cd
rodrigcasio <rod.casio@outlook.com>
2025-12-10 17:25:23 -0600

    `fix:` change cmdline placement

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- make cmdline like VIM at the bottom of the screen
+vim.opt.cmdheight = 2

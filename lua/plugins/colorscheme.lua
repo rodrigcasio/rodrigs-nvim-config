6b788eb2de6eafd80d447b88599959cfd426762d
rodrigcasio <rod.casio@outlook.com>
2025-12-10 23:04:30 -0600

    `feat:` Add new colorscheme settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
 
+-- colorscheme (fav: default (best),  sobert, darkblue, murphy, slate, elflord, default) 
 return {
   {
     "folke/tokyonight.nvim",
+    enable = false,   -- when wanting to use tokyo, delete this
     priority = 1000,
     opts = {
       style = "storm",
@@ -12,5 +14,22 @@ return {
         floats = "transparent",
       },
     },
+  },
+  
+  -- CURRENTLY IN USE: 
+  {
+    "LazyVim/LazyVim", 
+    priority = 1000, -- Ensures it runs last
+    config = function()
+      -- setting colorscheme
+      vim.cmd.colorscheme("default")
+      
+      vim.api.nvim_set_option("background", "dark") -- Set to dark for best contrast with the 'quiet' theme
+      vim.api.nvim_set_option("termguicolors", true) -- Required for modern color support
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      -- Optional: Apply transparency to the status line (lualine) and pop-ups
+      vim.api.nvim_set_hl(0, "LualineBg", { bg = "none" })
+    end,
   },
 }

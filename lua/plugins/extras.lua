178dbbba9e75272d1346d8f9c11173704854c933
rodrigcasio <rod.casio@outlook.com>
2025-12-16 20:35:44 -0600

    `feat:` Add comment to mute warning LSP suggestions

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -3,7 +3,7 @@
 
 return {
   -- Languages
-  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.python" },  -- to mute these LSP python warngins with :LspStop ruff/pyright OR :lua vim.diagnostic.enable(false)
   { import = "lazyvim.plugins.extras.lang.json" },
   -- NEW: JavaScript/TypeScript support
   { import = "lazyvim.plugins.extras.lang.typescript" },

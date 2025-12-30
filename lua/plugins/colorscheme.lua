3644c50bb8bc46574f619dd947fd0c61147c7f1a
rodrigcasio <rod.casio@outlook.com>
2025-12-30 16:56:54 -0600

    `fix:` Modify colorshemes settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
 
--- colorscheme (fav: default (best),  sobert, darkblue, murphy, slate, elflord, default) 
+-- colorscheme (fav: default (best),  sobert, darkblue, murphy, slate, elflord, default)
 return {
   {
     "folke/tokyonight.nvim",
-    enable = false,   -- when wanting to use tokyo, delete this
+    -- when wanting to use tokyo, delete this
+    -- enable = false,
     priority = 1000,
     opts = {
       style = "storm",
@@ -16,20 +17,21 @@ return {
     },
   },
 
-  -- CURRENTLY IN USE: 
-  {
-    "LazyVim/LazyVim",
-    priority = 1000, -- Ensures it runs last
-    config = function()
-      -- setting colorscheme
-      vim.cmd.colorscheme("default")
-
-      vim.api.nvim_set_option("background", "dark") -- Set to dark for best contrast with the 'quiet' theme
-      vim.api.nvim_set_option("termguicolors", true) -- Required for modern color support
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      -- Optional: Apply transparency to the status line (lualine) and pop-ups
-      vim.api.nvim_set_hl(0, "LualineBg", { bg = "none" })
-    end,
-  },
+  -- -- CURRENTLY IN USE:
+  -- {
+  --   "LazyVim/LazyVim",
+  --   -- ensures it runs last
+  --   -- priority = 1000,
+  --   config = function()
+  --     -- setting colorscheme
+  --     vim.cmd.colorscheme("default")
+  --
+  --     vim.api.nvim_set_option("background", "dark") -- Set to dark for best contrast with the 'quiet' theme
+  --     vim.api.nvim_set_option("termguicolors", true) -- Required for modern color support
+  --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --     -- Optional: Apply transparency to the status line (lualine) and pop-ups
+  --     vim.api.nvim_set_hl(0, "LualineBg", { bg = "none" })
+  --   end,
+  -- },
 }

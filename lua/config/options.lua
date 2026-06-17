ce46a4f88c5c4f45f816b579e8ac49d8a8d0a189
rodrigcasio <rod.casio@outlook.com>
2026-06-17 00:46:07 -0600

    `fix:` add enable windows `copy/paste` and fix artifact bug

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,21 @@ return {
     clipboard = "unnamedplus",
   },
 }
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.guicursor = ""
+
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw))',
+  },
+  cache_enabled = 0,
+}

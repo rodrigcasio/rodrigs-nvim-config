5e65497dcdbfca3e3577c642af29ab7b7bc84a96
rodrigcasio <rod.casio@outlook.com>
2026-06-17 00:56:20 -0600

    `fix:` include blueprint for `options.lua` fixing ghost characters

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,26 +3,25 @@
 -- Add any additional options here
 vim.g.lazyvim_check_order = false
 
-return {
-  opt = {
-    clipboard = "unnamedplus",
-  },
-}
-
 local opt = vim.opt
 
+-- 1. Your original clipboard choice + new options
+opt.clipboard = "unnamedplus"
 opt.relativenumber = true
+
+-- 2. Fix ghost characters by stopping tmux cursor shaping modifications
 opt.guicursor = ""
 
+-- 3. Seamless Windows Clipboard Sync
 vim.g.clipboard = {
-  name = 'WslClipboard',
+  name = "WslClipboard",
   copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
   },
   paste = {
-    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw))',
-    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw))',
+    ["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw))",
+    ["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw))",
   },
   cache_enabled = 0,
 }

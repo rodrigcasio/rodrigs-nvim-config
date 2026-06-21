fb841d5edb48d3e69b73806056237782b643c737
rodrigcasio <rod.casio@outlook.com>
2026-06-20 21:11:05 -0600

    `fix:` Remove `^M` when yanked and pasted code within LazyVim
    Also added backup packages installed in ArchWSL

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,14 +12,14 @@ opt.relativenumber = true
 -- Smart Clipboard: Detects if running on native Linux or Windows/WSL
 if vim.fn.has("wsl") == 1 then
   vim.g.clipboard = {
-    name = "WslClipboard",
+    name = "win32yank-wsl",
     copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
     },
     paste = {
-      ["+"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
-      ["*"] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
     },
     cache_enabled = 0,
   }

e9274f29469cf009290de550700219ad00ff14a2
rodrigcasio <rod.casio@outlook.com>
2026-06-21 23:09:33 -0600

    `fix:` removed abreviations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Command-line abreviation for safe buffer deletion
-
-vim.cmd([[
-  cnoreabbrev <expr> bdq (getcmdtype() == ':' && getcmdline() =~ '^bdq$') ? 'bn|bd#' : 'bdq'
-]])

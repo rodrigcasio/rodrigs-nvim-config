4e56e716baf9f0f1525f883804b88333fe1f10f2
rodrigcasio <rod.casio@outlook.com>
2026-06-21 22:43:54 -0600

    `feat:` Add abreviation `:bdq` to delete buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>bd", ":bn | bd #<CR>", { desc = "Delete buffer keep layout"})
+
+-- Command-line abreviation for safe buffer deletion
+
+vim.cmd([[
+  cnoreabbrev <expr> bdq (getcmdtype() == ':' && getcmdline() =~ '^bdq$') ? 'bn|bd#' : 'bdq'
+]])
